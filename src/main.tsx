e6351ef35bccf4ecc1bf98609bec48e650e10483
fifolio <firasdabbabi@gmail.com>
2026-02-05 17:03:11 +0100

    configs

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
 import App from './App.tsx'
+import './index.css'
+import './utils/i18.ts'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
